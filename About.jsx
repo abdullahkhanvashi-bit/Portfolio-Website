c149cfad902d5d0f4df36f89f8ba24525b80e03b
abdullahkhanvashi-bit <abdullahkhanvashi@gmail.com>
2026-06-15 16:50:19 +0530

    Add files via upload

---
+++ About.jsx
@@ -0,0 +1,18 @@
+function About() {
+  return (
+    <section
+      id="about"
+      className="container mt-5"
+    >
+      <h2>About Me</h2>
+
+      <p>
+        A BSC IT Graduate who is 
+        passionate about Full Stack 
+        Java Development
+      </p>
+    </section>
+  );
+}
+
+export default About;
---
+++ Resume.jsx
@@ -0,0 +1,19 @@
+function Resume() {
+  return (
+    <section className="container mt-5">
+
+      <h2>Resume</h2>
+
+      <a
+        href="/Abdullah_resume.pdf"
+        download
+        className="btn btn-success"
+      >
+        Download Resume
+      </a>
+
+    </section>
+  );
+}
+
+export default Resume;
